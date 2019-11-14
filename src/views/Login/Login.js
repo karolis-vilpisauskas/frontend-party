3dff652b3172789148fc75e76ba6790d28414c3e
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-14 23:53:05 +0200

    - Login Container - Logo White - SCSS variables

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,23 +1,25 @@
 import React from "react";
-// import { useDispatch } from "react-redux";
-// import { login, logout } from "../../store/actions/authActions";
+import { useDispatch } from "react-redux";
+import { login } from "../../store/actions/authActions";
+import "./Login.scss";
+import bgImage from "../../assets/wave.png";
+import LogoWhite from "../../components/LogoWhite";
 import LoginForm from "../../components/LoginForm";
 
 const Login = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // const dispatchLogin = () => {
-  //   dispatch(login("tesonet", "partyanimal"));
-  // };
-
-  // const dispatchLogout = () => {
-  //   dispatch(logout);
-  // };
+  const dispatchLogin = () => {
+    dispatch(login("tesonet", "partyanimal"));
+  };
 
   return (
-    <div>
-      <h1>Login page</h1>
-      <LoginForm />
+    <div
+      className="login-container"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <LogoWhite />
+      <LoginForm submitAction={dispatchLogin} />
     </div>
   );
 };
