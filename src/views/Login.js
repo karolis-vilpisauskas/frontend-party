ec79faad85140bc410a708828dd617067c1cb4d7
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-14 17:08:48 +0200

    Added base scss

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { login, logout } from "../store/actions/authActions";
-import { getServers, filterServers } from '../store/actions/serverActions';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -14,20 +13,10 @@ const Login = () => {
     dispatch(logout);
   };
 
-  const dispatchServers = () => {
-    dispatch(getServers());
-  }
-
-  const dispatchFilter = () => {
-    dispatch(filterServers('distance', false));
-  }
-
   return (
     <div>
       <button onClick={dispatchLogin}>Login</button>
       <button onClick={dispatchLogout}>Logout</button>
-      <button onClick={dispatchServers}>Servers</button>
-      <button onClick={dispatchFilter}>Filter</button>
     </div>
   );
 };
