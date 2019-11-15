90a9959c1d595bbfff2b7009aedea868801731ee
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-15 17:13:13 +0200

    - Added icons - Created server components boilerplate

---
+++ src/components/Header/LogoBlack/LogoBlack.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LogoBlack.scss";
+
+const LogoBlack = () => {
+  return <div></div>;
+};
+
+export default LogoBlack;
---
+++ src/components/ServerList/ServerList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ServerList.scss";
+
+const ServerList = () => {
+  return <div></div>;
+};
+
+export default ServerList;
