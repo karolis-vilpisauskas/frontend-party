ea9954f344ad515080620d0a6d22510c7eeef632
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-18 17:40:02 +0200

    Updates

---
+++ precache-manifest.e956759a421c96c1babfd401d2a68bf1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abe22a076dd487f010a9e1b38311a5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c43fe84eab3004f6888",
+    "url": "/static/css/main.e0102b7e.chunk.css"
+  },
+  {
+    "revision": "56eed34e24a53f15aff8",
+    "url": "/static/js/2.7eee58b0.chunk.js"
+  },
+  {
+    "revision": "5c43fe84eab3004f6888",
+    "url": "/static/js/main.aaa5663f.chunk.js"
+  },
+  {
+    "revision": "f649b18296e62814dce0",
+    "url": "/static/js/runtime-main.2c7d9cb5.js"
+  },
+  {
+    "revision": "967a93a7c5ab9df5079a27bc2b118cbe",
+    "url": "/static/media/ico-lock.967a93a7.svg"
+  },
+  {
+    "revision": "f95896fd7431f46a664219a7b15439ba",
+    "url": "/static/media/ico-logout.f95896fd.svg"
+  },
+  {
+    "revision": "7791427eb2b207c1faa420485b1e2f9a",
+    "url": "/static/media/ico-username.7791427e.svg"
+  },
+  {
+    "revision": "c3be98e3ce24bc817ff1cf467a8e42e5",
+    "url": "/static/media/wave.c3be98e3.png"
+  }
+]);
