00490aa6bfc39a785040ffe63e7d0205634dabec
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-18 12:01:23 +0200

    - Added not found page

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Page was not found</h1>
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Page was not found</h2>
+      <Link exact to="/">
+        Login
+      </Link>
     </div>
   );
 };
