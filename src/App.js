067a84aa1a77b37897c41a57f36fb71d31e897e8
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-18 04:21:30 +0200

    - Added loading spinner

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.scss";
 
 import ViewContainer from "./views/ViewContainer";
+import Spinner from "./components/Spinner";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Spinner />
       <ViewContainer />
     </BrowserRouter>
   );
---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Spinner.scss";
+const Spinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
