51efc9fb415b0602b25a1f89cc5b2a0fb43c6004
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-20 18:26:26 +0200

    - Header test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,33 +1,24 @@
-import "@testing-library/jest-dom/extend-expect";
 import React from "react";
+import ReactDOM from "react-dom";
 import { cleanup, render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Header from "./Header";
+
 afterEach(cleanup);
 
-const createTestProps = props => ({
-  ...props
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Header />, div);
 });
 
-const renderTest = () => {
-  const props = createTestProps();
+it("renders header correctly", () => {
   const { getByTestId } = render(
-    <Header {...props}>
-      <div data-testid="child" />
+    <Header>
+      <div data-testid="child-1"></div>
+      <div data-testid="child-2"></div>
     </Header>
   );
-  const container = getByTestId("header");
-  return {
-    getByTestId,
-    container
-  };
-};
 
-describe("Header", () => {
-  describe("rendering", () => {
-    test("it renders children crashing", () => {
-      const { container, getByTestId } = renderTest();
-      expect(container.children.length).toBe(1);
-      expect(getByTestId("child")).toBeDefined();
-    });
-  });
+  expect(getByTestId("header").children.length).toBe(2);
+  expect(getByTestId("child-1")).toBeDefined();
 });
