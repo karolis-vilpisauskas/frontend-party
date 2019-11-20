b9ee338fcbeea02ddd0ba232fa1cee63141d36cd
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-20 19:08:39 +0200

    - Input test

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,31 +1,38 @@
-import '@testing-library/jest-dom/extend-expect'
 import React from "react";
-import { cleanup, render } from "@testing-library/react";
+import ReactDOM from "react-dom";
+import { cleanup, render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Input from "./Input";
+
 afterEach(cleanup);
 
-const createTestProps = props => ({
-  ...props
+const createTestProps = () => ({
+  icon: "random-icon",
+  error: true,
+  type: "text",
+  placeholder: "username",
+  name: "username"
 });
 
-const renderTest = () => {
-  const props = createTestProps();
-  const { getByTestId } = render(
-    <Input {...props}>
-    </Input>
-  );
-  const container = getByTestId("input");
-  return {
-    getByTestId,
-    container
-  };
-};
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Input />, div);
+});
 
-describe("Error", () => {
-  describe("rendering", () => {
-    test("it renders without crashing", () => {
-      const { container } = renderTest();
-      expect(container).toMatchSnapshot();
-    });
-  });
+it("renders input correctly", () => {
+  const fn = jest.fn();
+  const props = createTestProps();
+  const { getByTestId } = render(<Input {...props} onChange={fn} />);
+  const input = getByTestId("input").childNodes[1];
+  const icon = getByTestId("input").firstChild;
+  const validationIcon = getByTestId("input").childNodes[2];
+
+  fireEvent.change(input, { target: { value: 'a' }});
+
+  expect(icon).toBeDefined();
+  expect(input).toHaveAttribute("type");
+  expect(input).toHaveAttribute("placeholder");
+  expect(input).toHaveAttribute("name");
+  expect(validationIcon.classList[4]).toBe("is-error");
+  expect(fn).toHaveBeenCalled();
 });
